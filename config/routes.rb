1f11a0f3c0ee6cf8aa4dbd57ffe601c354e66062
Ian MacLeod <ian@nevir.net>
2013-02-10 00:10:21 -0800

    OmniAuth wiring, even though it probably won't be used for a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Clarity::Application.routes.draw do
   root to: "home#home"
+
+  # Omniauth expects a 404ing endpoint; we set up a quick one for a named route
+  # http://stackoverflow.com/a/12117618/422263
+  match 'auth/:provider', to: proc { [404, {}, [""]] }, as: :authorize
+  # Handle the flip side
+  match 'auth/:provider/callback', to: 'sessions#create'
 end
